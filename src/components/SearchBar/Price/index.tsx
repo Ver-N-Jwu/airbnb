8b10a758c1078e994a1fb114ac4b543cc74e2e97
ver <luzverde0314@gmail.com>
2022-05-31 15:39:49 +0900

    ♻️ 그래프 완성 및 분리

--- src/components/SearchBar/Price/index.tsx
+++ src/components/SearchBar/Price/index.tsx
@@ -30,6 +30,7 @@ const Price = ({ modalOpen, setModalOpen }: props) => {
       {modalOpen === 2 && (
         <Modal setModalOpen={setModalOpen}>
           <PriceRangeGraph />
+          <Slider />
         </Modal>
       )}
     </>
@@ -41,102 +42,124 @@ const PriceRangeGraph = () => {
   const [maxPrice, setMaxPrice] = useState(0);
   const [average, setAverage] = useState(0);
 
+  //데이터 받는 로직 임시 주석화
+  // const priceArray: number[] = [];
+
+  // const roomsData = getData("fakeDB.json").then((json) => {
+  //   json.rooms.forEach((room: { salePrice: number }) => {
+  //     priceArray.push(room.salePrice);
+  //   });
+  //   priceArray.sort((a: number, b: number) => a - b);
+  //   setMinPrice(priceArray[0]);
+  //   setMaxPrice(priceArray[priceArray.length - 1]);
+  //   setAverage(
+  //     priceArray.reduce((acc: number, cur: number) => {
+  //       return acc + cur;
+  //     }, 0) / priceArray.length,
+  //   );
+  // });
+
+  //TODO: 데이터 받아오면 수정할 부분
+  useEffect(() => {
+    if (minPrice === 0 || maxPrice === 0 || average === 0) {
+      setMinPrice(Math.min(...mockArray.map(({ x }) => x)));
+      setMaxPrice(Math.max(...mockArray.map(({ x }) => x)));
+      setAverage(getAverage(mockArray) >> 0);
+    }
+  }, []);
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  const priceArray: number[] = [];
-
-  const mockArray = [
-    { x: 15000, y: 50 },
-    { x: 30000, y: 14 },
-    { x: 50000, y: 10 },
-    { x: 70000, y: 14 },
-    { x: 90000, y: 26 },
-    { x: 100000, y: 30 },
-    { x: 120000, y: 45 },
-    { x: 130000, y: 60 },
-    { x: 135000, y: 65 },
-    { x: 140000, y: 80 },
-    { x: 143000, y: 70 },
-    { x: 150000, y: 50 },
-    { x: 170000, y: 66 },
-    { x: 200000, y: 25 },
-    { x: 240000, y: 16 },
-    { x: 300000, y: 15 },
-    { x: 320000, y: 1 },
-  ];
-
-  const roomsData = getData("fakeDB.json").then((json) => {
-    json.rooms.forEach((room: { salePrice: number }) => {
-      priceArray.push(room.salePrice);
-    });
-    priceArray.sort((a: number, b: number) => a - b);
-    setMinPrice(priceArray[0]);
-    setMaxPrice(priceArray[priceArray.length - 1]);
-    setAverage(
-      priceArray.reduce((acc: number, cur: number) => {
-        return acc + cur;
-      }, 0) / priceArray.length,
-    );
-  });
-
   const canvas = canvasRef.current as HTMLCanvasElement;
   const context = canvas?.getContext("2d");
 
-  const drawGraph = (context: CanvasRenderingContext2D | null, mockArray: coordinates[]) => {
-    if (!context) return;
-    const x_max = Math.max(...mockArray.map((coordinate) => coordinate.x));
-    const y_max = Math.max(...mockArray.map((coordinate) => coordinate.y));
-    const x_divide = x_max / 365; //canvas width
-    const y_divide = y_max / 100; //canvas height
-    const coordinates = mockArray.map((coordinate) => {
-      return { x: coordinate.x / x_divide, y: 100 - coordinate.y / y_divide };
-    });
-
-    console.log(coordinates);
-
-    context?.beginPath();
-    context?.moveTo(0, 100); //(0, canvas height)
-    console.log(coordinates[0].x, coordinates[0].y);
-    context.strokeStyle = "lightgray";
-    context.fillStyle = "black";
-
-    for (let i = 0; i < coordinates.length - 1; i++) {
-      const x_mid = (coordinates[i].x + coordinates[i + 1].x) / 2;
-      const y_mid = (coordinates[i].y + coordinates[i + 1].y) / 2;
-      const cp_x1 = (x_mid + coordinates[i].x) / 2;
-      const cp_x2 = (x_mid + coordinates[i + 1].x) / 2;
-
-      context?.quadraticCurveTo(cp_x1, coordinates[i].y, x_mid, y_mid);
-      context?.quadraticCurveTo(cp_x2, coordinates[i + 1].y, coordinates[i + 1].x, coordinates[i + 1].y);
-
-      if (context) {
-        context?.stroke();
-      }
-    }
-
-    if (context) {
-      context?.fill();
-    }
-  };
-
   drawGraph(context, mockArray);
+  // const drawRange() => { }
 
   return (
     <>
+      {/* TODO: Description 컴포넌트로 변경 */}
       <div>가격 범위</div>
       <div>
-        {minPrice} - {maxPrice}+
+        {minPrice} - {maxPrice}
       </div>
-      <div>평균 1박 요금은 {average} 입니다.</div>
+      <div>평균 1박 요금은 {average}원 입니다.</div>
       <canvas width="365" height="100" ref={canvasRef} />
     </>
   );
 };
 
+const Slider = () => {};
+
 const getData = async (url: string) => {
   const data = await fetch(url);
 
   return data.json();
+};
+
+const mockArray = [
+  { x: 15000, y: 5 },
+  { x: 30000, y: 14 },
+  { x: 50000, y: 10 },
+  { x: 70000, y: 14 },
+  { x: 90000, y: 26 },
+  { x: 100000, y: 30 },
+  { x: 120000, y: 45 },
+  { x: 130000, y: 60 },
+  { x: 135000, y: 65 },
+  { x: 140000, y: 80 },
+  { x: 143000, y: 70 },
+  { x: 150000, y: 50 },
+  { x: 170000, y: 66 },
+  { x: 200000, y: 25 },
+  { x: 240000, y: 16 },
+  { x: 300000, y: 45 },
+  { x: 320000, y: 10 },
+  // { x: 350000, y: 40 },
+  // { x: 400000, y: 35 },
+  // { x: 600000, y: 30 },
+  // { x: 800000, y: 20 },
+  // { x: 900000, y: 10 },
+  // { x: 1000000, y: 1 },
+];
+
+const getAverage = (priceArray: coordinates[]) => {
+  const totalAmount = priceArray.map(({ y }) => y).reduce((acc, cur) => acc + cur, 0);
+  const totalPrice = priceArray.map(({ x, y }) => x * y).reduce((acc, cur) => acc + cur, 0);
+
+  return totalPrice / totalAmount;
+};
+
+const drawGraph = (context: CanvasRenderingContext2D | null, mockArray: coordinates[]) => {
+  if (!context) return;
+  const x_max = Math.max(...mockArray.map(({ x }) => x));
+  const x_min = Math.min(...mockArray.map(({ x }) => x));
+  const y_max = Math.max(...mockArray.map(({ y }) => y));
+  const x_ratio = (x_max - x_min) / 365; //canvas width
+  const y_ratio = y_max / 100; //canvas height
+  const coordinates = mockArray.map((coordinate) => {
+    return { x: (coordinate.x - x_min) / x_ratio, y: 100 - coordinate.y / y_ratio };
+  });
+
+  context.beginPath();
+  context.moveTo(coordinates[0].x, 100); //(최소가격, height)
+  context.lineTo(coordinates[0].x, coordinates[0].y); //시작점 처리
+  context.strokeStyle = "lightgray";
+  context.fillStyle = "black";
+
+  for (let i = 0; i < coordinates.length - 1; i++) {
+    const x_mid = (coordinates[i].x + coordinates[i + 1].x) / 2;
+    const y_mid = (coordinates[i].y + coordinates[i + 1].y) / 2;
+    const cp_x1 = (x_mid + coordinates[i].x) / 2;
+    const cp_x2 = (x_mid + coordinates[i + 1].x) / 2;
+
+    context.quadraticCurveTo(cp_x1, coordinates[i].y, x_mid, y_mid);
+    context.quadraticCurveTo(cp_x2, coordinates[i + 1].y, coordinates[i + 1].x, coordinates[i + 1].y);
+
+    context.stroke();
+  }
+  context.lineTo(coordinates[coordinates.length - 1].x, 100); //끝 점 처리
+
+  context.fill();
 };
 
 export default Price;

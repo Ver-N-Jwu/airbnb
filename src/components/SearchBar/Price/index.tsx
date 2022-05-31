36582005cc1cdc401035e13e3ca0f03d37f47bcb
ver <luzverde0314@gmail.com>
2022-05-31 10:30:35 +0900

    ✨ 가격 범위 그래프 구현

--- src/components/SearchBar/Price/index.tsx
+++ src/components/SearchBar/Price/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react";
+
 import X_ICON from "@assets/x-icon.svg";
 import Icon from "@components/common/Icon";
 import Modal from "@components/common/Modal";
@@ -7,6 +9,11 @@ import * as S from "./style";
 interface props {
   modalOpen: number;
   setModalOpen: React.Dispatch<React.SetStateAction<number>>;
+}
+
+interface coordinates {
+  x: number;
+  y: number;
 }
 
 const Price = ({ modalOpen, setModalOpen }: props) => {
@@ -34,7 +41,29 @@ const PriceRangeGraph = () => {
   const [maxPrice, setMaxPrice] = useState(0);
   const [average, setAverage] = useState(0);
 
-  const priceArray: number[] = [50000, 120000, 170000, 140000, 150000, 190000, 200000, 135000, 166000, 153000];
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const priceArray: number[] = [];
+
+  const mockArray = [
+    { x: 15000, y: 5 },
+    { x: 30000, y: 14 },
+    { x: 50000, y: 10 },
+    { x: 70000, y: 14 },
+    { x: 90000, y: 26 },
+    { x: 100000, y: 30 },
+    { x: 120000, y: 45 },
+    { x: 130000, y: 60 },
+    { x: 135000, y: 65 },
+    { x: 140000, y: 80 },
+    { x: 143000, y: 70 },
+    { x: 150000, y: 50 },
+    { x: 170000, y: 66 },
+    { x: 200000, y: 25 },
+    { x: 240000, y: 16 },
+    { x: 300000, y: 15 },
+    { x: 320000, y: 1 },
+  ];
 
   const roomsData = getData("fakeDB.json").then((json) => {
     json.rooms.forEach((room: { salePrice: number }) => {
@@ -50,6 +79,46 @@ const PriceRangeGraph = () => {
     );
   });
 
+  const canvas = canvasRef.current as HTMLCanvasElement;
+  const context = canvas?.getContext("2d");
+
+  const drawGraph = (context: CanvasRenderingContext2D | null, mockArray: coordinates[]) => {
+    if (!context) return;
+    const x_max = Math.max(...mockArray.map((coordinate) => coordinate.x));
+    const y_max = Math.max(...mockArray.map((coordinate) => coordinate.y));
+    const x_divide = x_max / 365;
+    const y_divide = y_max / 100;
+    const coordinates = mockArray.map((coordinate) => {
+      return { x: coordinate.x / x_divide, y: 100 - coordinate.y / y_divide };
+    });
+
+    context?.beginPath();
+    context?.moveTo(coordinates[0].x, coordinates[0].y);
+    console.log(coordinates[0].x, coordinates[0].y);
+    context.strokeStyle = "lightgray";
+    context.fillStyle = "black";
+
+    for (let i = 0; i < coordinates.length - 1; i++) {
+      const x_mid = (coordinates[i].x + coordinates[i + 1].x) / 2;
+      const y_mid = (coordinates[i].y + coordinates[i + 1].y) / 2;
+      const cp_x1 = (x_mid + coordinates[i].x) / 2;
+      const cp_x2 = (x_mid + coordinates[i + 1].x) / 2;
+
+      context?.quadraticCurveTo(cp_x1, coordinates[i].y, x_mid, y_mid);
+      context?.quadraticCurveTo(cp_x2, coordinates[i + 1].y, coordinates[i + 1].x, coordinates[i + 1].y);
+
+      if (context) {
+        context?.stroke();
+      }
+    }
+
+    if (context) {
+      context?.fill();
+    }
+  };
+
+  drawGraph(context, mockArray);
+
   return (
     <>
       <div>가격 범위</div>
@@ -57,19 +126,13 @@ const PriceRangeGraph = () => {
         {minPrice} - {maxPrice}+
       </div>
       <div>평균 1박 요금은 {average} 입니다.</div>
-      <canvas width="365" height="100"></canvas>
+      <canvas width="365" height="100" ref={canvasRef} />
     </>
   );
 };
 
 const getData = async (url: string) => {
   const data = await fetch(url);
-
-  return data.json();
-};
-
-const getRoomsData = async () => {
-  const data = await fetch("fakeDB.json");
 
   return data.json();
 };

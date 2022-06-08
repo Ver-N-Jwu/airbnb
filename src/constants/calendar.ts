a0eec2310603bf3a3c468c8945ec5f1dc1e82fff
ver <luzverde0314@gmail.com>
2022-06-08 10:57:43 +0900

    🚚 constants 파일 분리

--- src/constants/calendar.ts
+++ src/constants/calendar.ts
@@ -12,8 +12,5 @@ export enum CalendarTypes {
   ALL_REMOVE,
 }
 
-export const CANVAS_HEIGHT = 100;
-export const CANVAS_WIDTH = 365;
-export const BUTTON_RADIUS = 12;
 export const CALENDAR_PAGE = 2;
 export const HALF_MOVE_POINT = 50;

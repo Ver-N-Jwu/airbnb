87ee41ec2ad0aca2c86843b72fbd75b442b5a461
ver <luzverde0314@gmail.com>
2022-06-08 10:59:25 +0900

    🚧 PriceProvider 사용해서 상태 전달

--- src/components/SearchBar/Price/index.tsx
+++ src/components/SearchBar/Price/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-debugger */
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { useEffect } from "react";
 
 import X_ICON from "@assets/x-icon.svg";
 import Icon from "@components/common/Icon";
 import TextBox from "@components/common/TextBox";
+import { usePriceState } from "@contexts/PriceProvider";
 import { useSearchModalDispatch } from "@contexts/SearchModalProvider";
 
 import * as S from "./style";
@@ -11,15 +13,30 @@ import * as S from "./style";
 const Price = () => {
   const priceElement = useRef(null);
   const { onOpenSearchModal, onCloseSearchModal } = useSearchModalDispatch();
+  const { priceRange } = usePriceState();
+  const [text, setText] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!priceRange) return;
+    const text = `${priceRange?.minPrice}~${priceRange?.maxPrice}`;
+    setText(text);
+  }, [priceRange]);
 
   return (
     <>
       <S.Price onClick={() => onOpenSearchModal("PRICE")} ref={priceElement}>
-        <TextBox label="요금" placeholder="금액대 설정" textContent={null} />
+        <TextBox label="요금" placeholder="금액대 설정" textContent={validateText(text)} />
         {true && <Icon onClick={onCloseSearchModal} data-button="REMOVE" iconName={X_ICON} iconSize="base" />}
       </S.Price>
     </>
   );
 };
 
-export default Price;
+export default React.memo(Price);
+
+const validateText = (text: string | null): string | null => {
+  if (!text) {
+    return null;
+  }
+  return text;
+};

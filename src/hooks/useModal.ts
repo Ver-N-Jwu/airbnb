12c3d3a8adab6cd1df54dc8dc01698f8503d2fc8
ver <luzverde0314@gmail.com>
2022-06-09 19:27:24 +0900

    🔥 안쓰는 useModal삭제

--- src/hooks/useModal.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const useModal = (initialState: null) => {
-  const [modal, setModal] = useState(initialState);
-  return [modal, setModal];
-};
-
-export default useModal;
